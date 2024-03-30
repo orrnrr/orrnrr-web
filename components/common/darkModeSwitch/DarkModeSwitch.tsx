0d22f0297291c3d93479e59d2a1c748c473ef2fb
YukDDong <ekfhd5537@naver.com>
2024-03-30 17:15:31 +0900

    헤더에 다크모드 기능 추가

---
+++ components/common/darkModeSwitch/DarkModeSwitch.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "next-themes";
+
+export default function DarkModeSwitch() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <Switch
+      checked={theme === "dark"}
+      onCheckedChange={() => setTheme(theme === "dark" ? "light" : "dark ")}
+    />
+  );
+}
--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -1,9 +1,9 @@
-import { Switch } from "@/components/ui/switch";
 import Link from "next/link";
+import DarkModeSwitch from "../darkModeSwitch/DarkModeSwitch";
 
 export default function Header() {
   return (
-    <header className="bg-main w-full h-14 flex flex-col items-center justify-center">
+    <header className="bg-main w-full h-14 flex flex-col items-center justify-center fixed top-0 z-50">
       <div className="w-[1400px] flex justify-between items-center gap-4 px-4">
         <h1 className="flex-none text-2xl font-bold text-white">
           <Link href="/">OrrNrr</Link>
@@ -17,7 +17,7 @@ export default function Header() {
           </li>
         </ul>
         <div className="flex items-center">
-          <Switch />
+          <DarkModeSwitch />
         </div>
         <ul className="flex items-center gap-4 text-white">
           <li>
