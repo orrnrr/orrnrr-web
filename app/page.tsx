847c0e2a4d614a02b6e6758896195eaba5672b7d
YukDDong <ekfhd5537@naver.com>
2024-04-26 00:23:19 +0900

    메인페이지 작업중

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-"use client";
-
+import Chart from "@/components/chart/Chart";
+import Order from "@/components/order/Order";
 import RealTimeOrder from "@/components/realTimeOrder/RealTimeOrder";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <>
-      <Card className="w-[550px] px-4 py-6">
-        <RealTimeOrder />
-      </Card>
+      <div className="w-full flex justify-center bg-gray-200 pt-2">
+        <div className="w-[1400px] px-4 flex justify-around">
+          <div className="w-[1000px] flex flex-col gap-2">
+            <Card className="w-full px-4 py-6">
+              <Chart candleColor="orrnrr" />
+            </Card>
+            <div className="w-full flex gap-2">
+              <Card className="flex-1">
+                <RealTimeOrder />
+              </Card>
+              <Card className="flex-1">
+                <Order />
+              </Card>
+            </div>
+          </div>
+          <div className="w-[300px]">
+            <Card className="w-full h-96"></Card>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
